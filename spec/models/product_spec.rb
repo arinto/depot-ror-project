8d37cac82895f2e65dc115086e6f92ed089acd4e
Arinto Murdopo <arinto@gmail.com>
2013-03-29 19:02:41 +0100

    Finish iteration C1: Creating the Catalog Listing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,5 +10,28 @@ describe Product do
 		expect {create(:product, description: nil)}.to raise_error
 	end
 
-	#TODO: go through for each validation and write a test case on it	
+	it "new product with empty price" do
+		expect {create(:product, price: nil)}.to raise_error
+	end
+
+	it "new product with invalid price" do
+		expect {create(:product, price: 0.0)}.to raise_error
+	end
+
+	it "new produce with negative price" do
+		expect {create(:product, price: -1.0)}.to raise_error
+	end
+
+	it "new product with empty image url" do
+		expect {create(:product, image_url: nil)}.to raise_error
+	end
+
+	it "new product already exists" do
+		create(:product)
+		expect {create(:product)}.to raise_error
+	end
+
+	it "invalid image_url" do
+		expect {create(:product, image_url: ".docx")}.to raise_error
+	end
 end
