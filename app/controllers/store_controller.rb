a82b7cb4f9beaa0c834bacd743b7a78077fa9e6f
Arinto Murdopo <arinto@gmail.com>
2013-04-27 13:12:03 +0200

    Implement Task D's Playtime activities

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,12 @@ class StoreController < ApplicationController
   def index
   	#products will be instance variable to the view!
   	@products = Product.order(:title)
+  	@count = increment_count
+  	@shown_message = "You have been here #{@count} times" if @count > 5
+  end
+
+  def increment_count
+  	session[:counter] ||= 0
+  	session[:counter] += 1
   end
 end
